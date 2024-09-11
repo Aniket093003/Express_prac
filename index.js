1f2577c3a06b52249e525c9ac93b892ca0cf12f3
Aniket093003 <aniketkainth98105@gmail.com>
2024-09-12 00:15:25 +0530

    get request in express

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express";
+
+const app = express();
+
+const users = [
+  {
+    name: "aniket",
+    fever: [
+      {
+        health: false,
+      },
+    ],
+  },
+];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const anifever = users[0].fever;
+  const number = anifever.length;
+  let healthy = 0;
+  for (let i = 0; i < anifever.length; i++) {
+    if (anifever[i].health) {
+      healthy += 1;
+    }
+  }
+  const unHealthy = number - healthy;
+  res.json({
+    number,
+    healthy,
+    unHealthy,
+  });
+});
+
+app.post("/", (req, res) => {
+  const ishealthy = req.body.ishealthy;
+  users[0].fever.push({
+    health: ishealthy,
+  });
+  res.json({
+    msg: "done!",
+  });
+});
+
+
+app.put("/", (req, res) => {
+  for (let i = 0; i < users[0].fever.length; i++) {
+    users[0].fever[i].health = true;
+  }
+  res.json({});
+})
+
+app.delete("/", (req, res) => {
+    const newfever =  [];
+  for (let i = 0; i < users[0].fever.length; i++) {
+    if(users[0].fever[i].health){
+        newfever.push({
+            health: true
+        })
+    }
+  }
+  users[0].fever = newfever
+  res.json({});
+})
+
+app.listen(3000);
